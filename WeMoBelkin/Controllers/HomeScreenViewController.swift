a64e34e3719289cb586452f936ffe0615ce7b150
AdamZuspan <adam.zuspan@mobileappscompany.us>
2018-09-28 15:07:43 -0400

    testCommit

---
+++ WeMoBelkin/Controllers/HomeScreenViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  WeMoBelkin
+//
+//  Created by Admin on 9/26/18.
+//  Copyright © 2018 MobileAppsCompany. All rights reserved.
+//
+
+import UIKit
+import SVProgressHUD
+
+class HomeScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupLoadingProperties()
+        SVProgressHUD.show()
+        SVProgressHUD.dismiss(withDelay: 8) {
+            let secondHomeVC = self.storyboard?.instantiateViewController(withIdentifier: "SecondHomeScreen") as! SecondHomeScreenViewController
+            self.present(secondHomeVC, animated: false, completion: nil)
+        }
+    }
+    
+    func setupLoadingProperties() {
+        SVProgressHUD.setDefaultStyle(SVProgressHUDStyle.custom)
+        SVProgressHUD.setForegroundColor(.yellow)
+        SVProgressHUD.setRingRadius(16.0)
+        SVProgressHUD.setRingThickness(8.0)
+        SVProgressHUD.setCornerRadius(80.0)
+        SVProgressHUD.setBackgroundColor(.clear)
+        
+    }
+}
+
---
+++ WeMoBelkin/Controllers/SecondHomeScreenViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SecondHomeScreenViewController.swift
+//  WeMoBelkin
+//
+//  Created by Admin on 9/26/18.
+//  Copyright © 2018 MobileAppsCompany. All rights reserved.
+//
+
+import UIKit
+
+class SecondHomeScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+
+}
